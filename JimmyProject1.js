0e63bae8064daf7c2bb8aae481f1027d1663007b
James Vitola <jibbley99@gmail.com>
2022-10-25 13:45:44 -0400

    im learning

--- JimmyProject1.js
+++ JimmyProject1.js
@@ -1,16 +1,17 @@
-function numCheck() {
+function numCheck(testArray) {
   let arr = [];
-  for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
+  for (let i = testArray.length - 1; i >= 0; i--) {
+    if (testArray[i] % 3 === 0 && testArray[i] % 5 === 0) {
       arr.push("Ian Zygmunt");
-    } else if (i % 3 === 0) {
+    } else if (testArray[i] % 3 === 0) {
       arr.push("Ian");
-    } else if (i % 5 === 0) {
+    } else if (testArray[i] % 5 === 0) {
       arr.push("Zygmunt");
     } else {
-      arr.push(i);
+      arr.push(testArray[i]);
     }
   }
   return arr;
 }
-console.log(numCheck());
+let x = [1, 2, 5, 7, 12, 15, 19];
+console.log(numCheck(x));
---
+++ test.js
@@ -0,0 +1,6 @@
+function foo(x, y) {
+  return x + y;
+}
+console.log(foo(3, 8));
+console.log(foo(5, 5));
+console.log(foo(1, 2));
