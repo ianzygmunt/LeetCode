9c1fa19dd39d870afa412d77e53fc2bdbd5de67f
James Vitola <jibbley99@gmail.com>
2022-10-25 13:09:18 -0400

    i did it dad

--- JimmyProject1.js
+++ JimmyProject1.js
@@ -1,12 +1,16 @@
-for( var i = 1; i <= 100 ; i++) {
-    if(i % 3 === 0 && i % 5 === 0) {
-        console.log('Ian Zygmunt')
+function numCheck() {
+  let arr = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("Ian Zygmunt");
+    } else if (i % 3 === 0) {
+      arr.push("Ian");
+    } else if (i % 5 === 0) {
+      arr.push("Zygmunt");
+    } else {
+      arr.push(i);
     }
-    else if(i % 3 === 0 && !i % 5 === 0) {
-        console.log('Ian')
-    }
-    else if(!i % 3 === 0 && i % 5 === 0){
-        console.log('Zygmunt')
-    }
-    else{console.log(i)}
+  }
+  return arr;
 }
+console.log(numCheck());
